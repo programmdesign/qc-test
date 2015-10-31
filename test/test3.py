3b588dc0578b47284959c9cc6e3ed5b4983255bc
Christoph Neumann <mail@christoph-neumann.de>
2015-10-31 23:40:24 +0100

    Added subfolders for .checkignore test

---
+++ test/test3.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+variable = "testing"
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein %s" % variable
+print "das ist ein %s" % variable
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das %s" % "ist"
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 5 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 4 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 3 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 2 more
+eval("print variable")
+eval("print variable")
+##### 1 more
+eval("print variable")
+##### 3 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 2 more
+eval("print variable")
+eval("print variable")
+##### 4 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 5 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 4 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 5 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 4 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 3 more
+eval("print variable")
+eval("print variable")
+eval("print variable")
+##### 2 more
+eval("print variable")
+eval("print variable")
+##### 1 more
+eval("print variable")
