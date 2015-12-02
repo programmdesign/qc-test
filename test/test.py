c6020fed962cc3c116a71b1c0879858910cab138
Cody <cody@quantifiedcode.com>
2015-12-02 23:45:24 +0000

    migrated `%` string formating

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
@@ -14,87 +14,87 @@ exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
-eval("print variable")
-eval("print variable")
-variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
-exec(variable)
-eval(varialbe)
-eval(variable)
-eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
-eval("print variable")
-variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
-exec(variable)
-eval(varialbe)
-eval(variable)
-eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 variable = "testing"
-print "das ist ein %s" % variable
-print "das ist ein %s" % variable
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
 exec(variable)
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 eval(varialbe)
 eval(variable)
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 eval("print variable")
-print "das %s" % "ist"
+print "das {0!s}".format("ist")
 eval("print variable")
 eval("print variable")
 eval(varialbe)
