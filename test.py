ab507d5a4d48aefa63503e7c1d48744956fc21bc
Christoph Neumann <programmdesign@users.noreply.github.com>
2015-12-03 09:47:02 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,34 @@
 def test(a, b=[]):
   pass
 
+any([i*2 == 12 for i in range(10)])
+all([i*2 < 10 for i in range(10)])
+
+doubled_squares = [i*2 for i in [i*i for i in range(10)]]
+
+if number == None:
+
+times = [
+  {"hour": 8, "minute": 37, "second": 05, "hour": 12},
+  {"hour": 9, "minute": 37, "second": 05, "hour": 12}  
+]
+stuff_dict = dict([(time.minute, time.hour) for t in times])
+stuff_set = set([time.hour for t in times]
+
+some_list = None
+if not some_list is None:
+   do_something()
+   
+print "{} is {}".format("life", "hard")
+   
+time = {"hour": 8, "minute": 37, "second": 05, "hour": 12}
+print time
+# Output: {'second': 5, 'minute': 37, 'hour': 12}
+
+i, k = 1, 2
+if i <> k:
+  print("Unequal")
+
 id = "testing"
 variable = "testing"
 print "das ist ein {0!s}".format(variable)
