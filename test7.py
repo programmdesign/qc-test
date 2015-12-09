06dfed46741555a3afd0029cfed8e16739f4d68f
Christoph Neumann <mail@christoph-neumann.de>
2015-12-09 10:50:54 +0100

    Added more bugs

---
+++ test7.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "das ist ein %s" % "Test"
+
+def test(a, b=[]):
+    pass
+
+any([i*2 == 12 for i in range(10)])
+all([i*2 < 10 for i in range(10)])
+
+doubled_squares = [i*2 for i in [i*i for i in range(10)]]
+
+times=[]
+times.append({"hour": 8, "minute": 37, "second": 05, "hour": 12})
+times.append({"hour": 9, "minute": 37, "second": 05, "hour": 12})
+
+stuff_dict = dict([(t["minute"], t["hour"]) for t in times])
+stuff_set = set([t["hour"] for t in times])
+
+some_list = None
+if not some_list is None:
+    do_something()
+
+print "{} is {}".format("life", "hard")
+
+time = {"hour": 8, "minute": 37, "second": 05, "hour": 12}
+print time
+# Output: {'second': 5, 'minute': 37, 'hour': 12}
+
+i, k = 1, 2
+if i <> k:
+    print("Unequal")
+  
+x = None
+if x == None:
+  print "Ouch"
+
+id = "testing"
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+eval("print variable")
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+variable = "testing"
+print "das ist ein {0!s}".format(variable)
+print "das ist ein {0!s}".format(variable)
+exec(variable)
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+eval(varialbe)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+eval(varialbe)
+eval(variable)
+eval(variable)
+eval("print variable")
+print "das {0!s}".format("ist")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
+eval("print variable")
